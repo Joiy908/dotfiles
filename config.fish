7f728a50d6465ffe2b567c8a14921fe3c5403b87
joiy908 <joiy9088@gmail.com>
2023-08-27 20:38:40 +0800

    add config.fish

---
+++ config.fish
@@ -0,0 +1,56 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_prompt --description 'Write out the prompt'
+    set_color cyan
+    echo -n "╭─"
+    set_color normal
+
+    # Display current directory
+    set_color blue
+    echo -n (prompt_pwd)
+    set_color normal
+
+    # Add newline and prompt symbol
+    set_color cyan
+    echo
+    echo -n "╰─❯ "
+    set_color normal
+end
+
+# Alias definitions
+alias ll='ls -lah'
+alias la='ls -A'
+alias l='ls'
+alias cp='cp -i'
+alias mv='mv -i'
+alias v='vim'
+alias sl='ls'
+alias gl='git log --all --graph --decorate --oneline'
+alias gs='git status'
+alias gc='git commit'
+alias acenv='source .venv/bin/activate'
+alias go186='cd /mnt/d/src/etc/CS186'
+alias go110='cd /mnt/d/src/etc/CS110L'
+alias gong='cd /usr/local/nginx'
+
+# Functions
+# Define 'here' function
+function here
+    if test (count $argv) -eq 1
+        set loc (realpath $argv[1])
+    else
+        set loc (realpath ".")
+    end
+    ln -sfn "$loc" "$HOME/.shell.here"
+    echo "here -> "(readlink $HOME/.shell.here)
+end
+
+# Set 'there' variable
+set there "$HOME/.shell.here"
+
+# Define 'there' function
+function there
+    cd (readlink "$there")
+end
